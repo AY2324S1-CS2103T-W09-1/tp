f3af4d6a56e040af3ec2da53982cdc521ddb4d2a
bhnuka <01bhanukaekanayake@gmail.com>
2023-09-30 23:22:32 +0800

    Add RemarkCommand Implementation
    The address book app lacks the ability to add and edit remarks
    
    Users need to manage remarks for better organization
    
    Introduce RemarkCommand for adding/editing remarks
    
    Follows conventions for new commands, with parsing and model updates
    
    Includes tests to ensure RemarkCommand correctness

---
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -0,0 +1,8 @@
+package seedu.address.commons.core;
+
+/**
+ * Contains commonly returned messages
+ */
+public class Messages {
+    public static final String MESSAGE_INVALID_COMMAND_FORMAT = "Command format is invalid";
+}
